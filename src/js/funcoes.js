c98a81e90a9403cb17a77ec231627ff173ffc4bf
EricLamounier <eric_lamounier@outlook.com>
2022-10-14 20:17:57 -0300

    remove q

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -22,9 +22,6 @@ function cadastrar(){
         body: JSON.stringify(_data),
         headers: {"Content-type": "application/json; charset=UTF-8"}
       })
-      .then(document.getElementById("nome-produto").innerHTML = " ",
-            document.getElementById("descricao-produto").value = " ",
-            document.getElementById("valor-produto").value = " ");
 
       alert('Cadastrado com sucesso!')
       console.log(_data)
